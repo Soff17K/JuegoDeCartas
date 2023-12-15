420dd38d04b32cfccf4d85bb61d53ea7330e2c6b
Soff17K <sofitag09@gmail.com>
2023-12-15 11:13:39 -0600

    crear carpeta con las clases

---
+++ JuegoDeCartas/Clases/Deck.cs
@@ -0,0 +1,71 @@
+﻿using JuegoDeCartas.Enumeradores;
+using JuegoDeCartas.Interfaces;
+using JuegoDeCartas.Poker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuegoDeCartas.Clases
+{
+    public class Deck : IDeckDeCartas
+    {
+        private List<ICarta> _deck;
+
+        public Deck()
+        {
+            _deck = new List<ICarta>();
+
+            var figuras = Enum.GetValues(typeof(FigurasCartasEnum)).Cast<FigurasCartasEnum>().ToList();
+            var valores = Enum.GetValues(typeof(ValoresCartasEnum)).Cast<ValoresCartasEnum>().ToList();
+
+            foreach (var figura in figuras)
+            {
+                foreach (var valor in valores)
+                {
+                    _deck.Add(new Carta(figura, valor));
+                }
+            }
+        }
+
+        public void BarajearDeck()
+        {
+            var rnd = new Random();
+            _deck = _deck.OrderBy(x => rnd.Next()).ToList();
+        }
+
+        public void MeterCarta(ICarta carta)
+        {
+            if (carta == null)
+            {
+                throw new ArgumentNullException("La carta a meter no puede ser nula");
+            }
+
+            _deck.Add(carta);
+        }
+
+        public void MeterCarta(List<ICarta> cartas)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICarta SacarCarta(int indiceCarta)
+        {
+            if (indiceCarta < 0 || indiceCarta >= _deck.Count)
+            {
+                throw new ArgumentOutOfRangeException("Índice fuera del rango del mazo");
+            }
+
+            var carta = _deck[indiceCarta];
+            _deck.RemoveAt(indiceCarta);
+
+            return carta;
+        }
+
+        public ICarta VerCarta(int indiceCarta)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
