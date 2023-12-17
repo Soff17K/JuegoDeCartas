3100229e225bb62844f9965bcb74588c52fcb86d
ᓚᘏᗢ !? <datamayo.211@gmail.com>
2023-12-17 12:57:05 -0600

    Idea de implementación de color, escalera y trio

--- JuegoDeCartas/Poker/JuegoPoker.cs
+++ JuegoDeCartas/Poker/JuegoPoker.cs
@@ -104,6 +104,12 @@ namespace JuegoDeCartas.Poker
 
             var full = ((mismoNumero /*3*/ && mismoNumero /*2*/) && mismoPalo);
 
+            var color = mismoPalo;
+
+            var escalera = (escalera9a5 || escalera8a4 || escalera7a3 || escalera6a2 || escalera5aAs);
+            
+            var trio = (mismoNumero /*3*/);
+
             for (int i = 0; i < jugadores.Count; i++)
             {
                 foreach (var mano in jugadores){
